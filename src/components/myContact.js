b356d4ae7588fa3eeb4079ec884d6bf245112819
bamsi <=>
2023-09-06 18:05:37 +0300

    add form validation

--- src/components/myContact.js
+++ src/components/myContact.js
@@ -7,8 +7,59 @@ import Grid from "@mui/material/Grid";
 import MuiTexField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { Divider } from "@mui/material";
+import { useState } from "react";
+import axios from "axios";
+import Alert from "@mui/material/Alert";
 
 const MyContact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [validEmail, setValidEmail] = useState(false);
+
+  const validateEmail = (email) => {
+    const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    return regex.test(email);
+  };
+
+  const validateForm = () => {
+    return name !== "" || email !== "" || message !== "";
+  };
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+    validateEmail(email) ? setValidEmail(true) : setValidEmail(false);
+  };
+  const handleMessageChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    if (validateForm()) {
+      try {
+        const payload = { name, email, message };
+        const response = await axios.post(
+          "https://formspree.io/f/mjvlodlr",
+          payload
+        );
+        if (response.status === 200) {
+          setSuccessMessage("Thank you for contacting me!");
+        } else {
+          setErrorMessage("Woops!! Something went wrong.. Please try again");
+        }
+      } catch (error) {
+        setErrorMessage("Woops!! Something went wrong.. Please try again");
+      }
+    } else {
+      setErrorMessage("Woops!! Please fill all required fields");
+    }
+  };
   return (
     <Box style={{ marginTop: "3rem" }}>
       <Typography variant="h4" gutterBottom>
@@ -36,43 +87,71 @@ const MyContact = () => {
           </Typography>
         </div>
         <div>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <FormControl fullWidth sx={{ m: 1 }} variant="standard">
-                <InputLabel htmlFor="full name">Full Name</InputLabel>
-                <Input id="full-name" />
-              </FormControl>
-            </Grid>
-            <Grid item xs={12}>
-              <FormControl fullWidth sx={{ m: 1 }} variant="standard">
-                <InputLabel htmlFor="email">Email Address</InputLabel>
-                <Input id="email" />
-              </FormControl>
-            </Grid>
-            <Grid item xs={12}>
-              <FormControl fullWidth sx={{ m: 1 }} variant="standard">
-                <MuiTexField fullWidth multiline rows={3} />
-              </FormControl>
-            </Grid>
-            <Grid item xs={12}>
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "flex-start",
-                  alignContent: "center",
-                  paddingLeft: "5px",
-                }}
-              >
-                <Button
-                  variant="contained"
-                  color="success"
-                  style={{ width: "200px" }}
+          <form onSubmit={handleOnSubmit}>
+            <div>
+              {successMessage !== "" && (
+                <Alert severity="success">{successMessage}</Alert>
+              )}
+              {errorMessage !== "" && (
+                <Alert severity="error">{errorMessage}</Alert>
+              )}
+            </div>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <FormControl fullWidth sx={{ m: 1 }} variant="standard">
+                  <InputLabel htmlFor="full name">Full Name</InputLabel>
+                  <Input
+                    id="full-name"
+                    value={name}
+                    onChange={handleNameChange}
+                    required
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12}>
+                <FormControl fullWidth sx={{ m: 1 }} variant="standard">
+                  <InputLabel htmlFor="email">Email Address</InputLabel>
+                  <Input
+                    id="email"
+                    value={email}
+                    onChange={handleEmailChange}
+                    error={!validEmail}
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12}>
+                <FormControl fullWidth sx={{ m: 1 }} variant="standard">
+                  <MuiTexField
+                    fullWidth
+                    multiline
+                    rows={3}
+                    value={message}
+                    onChange={handleMessageChange}
+                    required
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12}>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "flex-start",
+                    alignContent: "center",
+                    paddingLeft: "5px",
+                  }}
                 >
-                  Submit
-                </Button>
-              </div>
+                  <Button
+                    variant="contained"
+                    color="success"
+                    style={{ width: "200px" }}
+                    type="submit"
+                  >
+                    Submit
+                  </Button>
+                </div>
+              </Grid>
             </Grid>
-          </Grid>
+          </form>
         </div>
       </Box>
     </Box>
